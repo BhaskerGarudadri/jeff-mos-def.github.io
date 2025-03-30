65a80ae940eaa9119fe99a6629a5f173a3c65c5a
Jeff <61635774+jeff-mos-def@users.noreply.github.com>
2025-03-30 08:52:13 -0500

    page name update and content date check

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "PMP Notes",
+    pageTitle: "PMP Notes+",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
